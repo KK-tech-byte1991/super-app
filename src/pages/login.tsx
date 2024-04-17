aded74af97c9333836942396da3d32b899c3d500
KK-tech-byte1991 <84914086+KK-tech-byte1991@users.noreply.github.com>
2024-04-17 22:43:05 +0530

    first commit

---
+++ src/pages/login.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import main from "../assets/main.png"
+import styles from "./login.module.css"
+
+const Login = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.imageContainer}>
+        <div className={styles.textContainer}>
+          <p className={styles.textHeading}>Discover new things on Superapp</p>
+        </div>
+        <img src={main} className={styles.mainImage} />
+
+      </div>
+      <div className={styles.loginContainer}>
+        
+          <p className={styles.heading}>Super app</p>
+          <p className={styles.subTitle}>Create your new account</p>
+          <input className={styles.loginInput} type="text" placeholder='Name'/><br/><br/>
+          <input className={styles.loginInput} type="text" placeholder='UserName'/><br/><br/>
+          <input className={styles.loginInput} type="email" placeholder='Email'/><br/><br/>
+          <input className={styles.loginInput} type="number" placeholder='Mobile'/><br/><br/>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Login
